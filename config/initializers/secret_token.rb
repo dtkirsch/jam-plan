820f16eb3d2a844bae0d047b084c6962173331fa
David Tresner-Kirsch <davidtk@mitre.org>
2013-05-31 15:46:58 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamPlan::Application.config.secret_token = 'b29f5e9d36277be69b85d828462761ccb4e79c9dbadb66d0d38b2562f0aa7d7d63e1a6d22c7c97c9196d51b60e5857fd6b71c4236a707ce69726baa51d701a0d'
