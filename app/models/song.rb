fd6ed31526f303086054a97c4fffa0b66ddf18d8
David Tresner-Kirsch <davidtk@mitre.org>
2013-06-04 18:15:55 -0400

    add some more songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,8 +9,8 @@ class Song < ActiveRecord::Base
   def self.populate
     files = Dir.glob("app/assets/pdfs/*").map{|f| File.basename(f)}
     songs = {}
-    files.map{|f| f.sub(/ \(.*\)_Tango.pdf/, "").downcase}.uniq.each{|s| songs[s]={}}
-    files.each{|f| name=f.sub(/ \(.*\)_Tango.pdf/, "").downcase; type=f.sub(/.*\(/,'').sub(/\).*/,'').downcase; songs[name][type]=f}
+    files.map{|f| f.sub(/ \(.*\)_[A-Za-z]*.pdf/, "").downcase}.uniq.each{|s| songs[s]={}}
+    files.each{|f| name=f.sub(/ \(.*\)_[A-Za-z]*.pdf/, "").downcase; type=f.sub(/.*\(/,'').sub(/\).*/,'').downcase; songs[name][type]=f}
     songs.each{|name, files| Song.new(:name=>name, :trebleC=>files['c'], :bassC=>files['bass clef'], :trebleBb=>files['bb']).save!}
   end
 end
