fd08d24110f4f1896d9d5755eb93d888ff67c519
David Tresner-Kirsch <davidtk@mitre.org>
2013-06-05 11:44:46 -0400

    date and time formatting

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 JamPlan::Application.initialize!
+
+# Date/Time formatting:
+Time::DATE_FORMATS[:clock_time] = "%I:%M %p"
